32c519975ef271b8d86a283abd97d3b998dbdde1
htavakkoli1368 <32728223+htavakkoli1368@users.noreply.github.com>
2026-08-08 22:32:45 +0330

    Add project files.

---
+++ TaskManagementFa/Controllers/TasksController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskManagementFa.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            var tasks = new List<string>
+            {
+                "Task 1",
+                "Task 2",
+                "Task 3"
+            };
+            return Ok(tasks);
+        }
+    }
+}
